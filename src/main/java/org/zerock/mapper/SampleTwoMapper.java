0988b4ec62ee8140e621a82e407660a109714e08
Alice5855 <105841246+Alice5855@users.noreply.github.com>
2022-08-17 10:18:39 +0900

    Testing branch commit
    Plz work

--- src/main/java/org/zerock/mapper/SampleTwoMapper.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.mapper;
-
-import org.apache.ibatis.annotations.Insert;
-
-public interface SampleTwoMapper {
-	@Insert("insert into tbl_sample2 (col2) values(#{data})")
-	public int insertCol2(String data);
-	// col2 is varchar2 50bytes
-}
