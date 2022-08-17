be42448ec99a7d612792d7623da25c721444f5c9
KJW0427 <lakers04271217@gmail.com>
2022-08-17 10:55:00 +0900

    USERSERVICETEST add

--- src/test/java/org/zerock/service/UserServiceImplTest.java
+++ src/test/java/org/zerock/service/UserServiceImplTest.java
@@ -1,14 +1,21 @@
 package org.zerock.service;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zerock.domain.UserVO;
+import org.zerock.mapper.UserMapper;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
+
+
+
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,39 +28,45 @@ public class UserServiceImplTest {
 	
 	
 	
-	@Test
-	public void testRegister() {
-		log.info("시작");
-		UserVO user = new UserVO();
-		log.info("중간");
-		user.setU_Email("junitTest@test.com");
-		user.setU_Name("Test00");
-		user.setU_pw("junit0816");
-		user.setU_Address("junit주소정보");
-		user.setU_gender("성별정보");
-		user.setU_profile_path("junit이미지경로");
-		userService.register(user);
-		log.info(user + " 로그 찍어보기 ");
-	}
+//	@Test
+//	public void testRegister() {
+//		log.info("시작");
+//		UserVO user = new UserVO();
+//		log.info("중간");
+//		user.setU_Email("junit@test.net");
+//		user.setU_Name("Test123");
+//		user.setU_pw("junit0817");
+//		user.setU_Address("junit0817");
+//		user.setU_gender("0817");
+//		user.setU_profile_path("junit이미지0817");
+//		userService.register(user);
+//		log.info(user + " 로그 찍어보기 ");
+//	}
 
 //	@Test
 //	public void testGet() {
 //		fail("Not yet implemented");
 //	}
-//
-//	@Test
-//	public void testModify() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testRemove() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testUserServiceImpl() {
-//		fail("Not yet implemented");
-//	}
+
+	@Test
+	public void testModify() {
+		UserVO user = userService.get("junit@test.net");
+		if (user == null) {
+			return;
+		} else {
+			user.setU_Address("junit수정테스트0817_두번째");;
+		}
+		
+		log.info("수정 결과 : " + userService.modify(user));
+		
+		
+	}
+
+	@Test
+	public void testRemove() {
+		log.info("삭제 결과 : " + userService.remove("testEmail"));
+	}
+
+
 
 }
